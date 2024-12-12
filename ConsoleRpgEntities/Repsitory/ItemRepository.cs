1567fa43757dd9a05aff5d2ceeb24499aeb6e5e5
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-12-12 15:16:27 -0500

    Add files via upload

---
+++ ConsoleRpgEntities/Repsitory/ItemRepository.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleRpgEntities.Data;
+using ConsoleRpgEntities.Models.Equipments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRpgEntities.Repsitory
+{
+    public class ItemRepository
+    {
+        private readonly GameContext _context;
+        public ItemRepository(GameContext context) { _context = context; }
+
+        // Update an item
+        public void UpdateItem(Item item)
+        {
+            _context.Items.Update(item);
+            _context.SaveChanges();
+        }
+    }
+}
