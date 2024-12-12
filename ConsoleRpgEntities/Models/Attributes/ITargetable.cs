b485853575b1555e8cd36c406a985b0a63d769a3
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-12-11 20:15:06 -0500

    Update ITargetable.cs

--- ConsoleRpgEntities/Models/Attributes/ITargetable.cs
+++ ConsoleRpgEntities/Models/Attributes/ITargetable.cs
@@ -1,7 +1,14 @@
-﻿namespace ConsoleRpgEntities.Models.Attributes;
+﻿using ConsoleRpgEntities.Models.Abilities.PlayerAbilities;
+using ConsoleRpgEntities.Models.Equipments;
+
+namespace ConsoleRpgEntities.Models.Attributes;
 
 public interface ITargetable
 {
     string Name { get; set; }
+    string Race { get; set; }
     int Health { get; set; }
+    string? Class { get; set; }
+    ICollection<Ability>? Abilities { get; set; }
+    Equipment? Equipment { get; set; }
 }
