27c71d4a2d0568143feea17b17202a0645a93ff1
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-12-11 20:23:09 -0500

    Update PlayerService.cs

--- ConsoleRpgEntities/Services/PlayerService.cs
+++ ConsoleRpgEntities/Services/PlayerService.cs
@@ -1,10 +1,11 @@
-﻿using ConsoleRpgEntities.Models.Abilities.PlayerAbilities;
+using ConsoleRpgEntities.Models.Abilities.PlayerAbilities;
 using ConsoleRpgEntities.Models.Attributes;
 using ConsoleRpgEntities.Models.Characters;
 using ConsoleRpgEntities.Models.Equipments;
+using ConsoleRpgEntities.Models.Rooms;
 using ConsoleRpgEntities.Services;
 
-public class PlayerService 
+public class PlayerService
 {
     private readonly IOutputService _outputService;
     private readonly AbilityService _abilityService;
@@ -17,15 +18,87 @@ public class PlayerService
 
     public void Attack(IPlayer player, ITargetable target)
     {
-        if (player.Equipment?.Weapon == null)
+        // "Dice roll"
+        Random rand = new Random();
+        int roll = rand.Next(1, 7);
+
+        // Stats
+        int attack = 0;
+        int playerDefense = 0;
+        int monsterDefense = 0;
+
+        // Monster defense stats
+        if (target.Race.Equals("Balrog"))
         {
-            _outputService.WriteLine($"{player.Name} has no weapon equipped!");
-            return;
+            monsterDefense = 19;
+        }
+        else if (target.Race.Equals("Fallen"))
+        {
+            monsterDefense = 18;
+        }
+        else if (target.Race.Equals("Goblin"))
+        {
+            monsterDefense = 5;
+        }
+        else if (target.Race.Equals("Kumiho"))
+        {
+            monsterDefense = 10;
+        }
+        else if (target.Race.Equals("Ogre"))
+        {
+            monsterDefense = 11;
+        }
+        else if (target.Race.Equals("Orrok") || target.Race.Equals("Troll"))
+        {
+            monsterDefense = 15;
+        }
+        else if (target.Race.Equals("Orrok"))
+        {
+            monsterDefense = 16;
+        }
+        else if (target.Race.Equals("Uruk"))
+        {
+            monsterDefense = 7;
         }
 
-        _outputService.WriteLine($"{player.Name} attacks {target.Name} with a {player.Equipment.Weapon.Name} dealing {player.Equipment.Weapon.Attack} damage!");
-        target.Health -= player.Equipment.Weapon.Attack;
-        _outputService.WriteLine($"{target.Name} has {target.Health} health remaining.");
+        if (player.Equipment.Armor != null)
+        {
+            playerDefense = player.Equipment.Armor.Defense;
+        }
+        if (player.Equipment.Weapon != null)
+        {
+            attack = player.Equipment.Weapon.Attack;
+
+            // Damage calculations
+            decimal damage = (roll + attack) / ((monsterDefense + 100) / 100);
+
+            // Total damage
+            int totalDamage = (int)Math.Round(damage, MidpointRounding.AwayFromZero);
+            int overkill = totalDamage + (target.Health - totalDamage);
+
+            // Player-specific attack logic
+            _outputService.WriteLine($"{player.Name} attacks {target.Name} with a {player.Equipment.Weapon.Name}!");
+            target.Health -= totalDamage;
+            if (target.Health > 0)
+            {
+                _outputService.WriteLine($"{target.Name} took {totalDamage} damage.");
+            }
+            else
+            {
+                if (target.Health - totalDamage < 0)
+                {
+                    _outputService.WriteLine($"{target.Name} took {overkill} damage.\n{target.Name} has been defeated.");
+                }
+                else if (target.Health - totalDamage == 0)
+                {
+                    _outputService.WriteLine($"{target.Name} took {totalDamage} damage.\n{target.Name} has been defeated.");
+                }
+            }
+        }
+        else
+        {
+            _outputService.WriteLine($"{player.Name} has no weapon equipped!");
+        }
     }
 
     public void UseAbility(IPlayer player, IAbility ability, ITargetable target)
@@ -49,6 +122,43 @@ public class PlayerService
         else
         {
             _outputService.WriteLine($"{player.Name} does not have the item {item.Name} in their inventory!");
+        }
+    }
+
+    public void Move(IPlayer player, string direction)
+    {
+        Room nextRoom = new Room();
+
+        if (direction == "north")
+        {
+            nextRoom = player.Room.North;
+        }
+        else if (direction == "south")
+        {
+            nextRoom = player.Room.South;
+        }
+        else if (direction == "east")
+        {
+            nextRoom = player.Room.East;
+        }
+        else if (direction == "west")
+        {
+            nextRoom = player.Room.West;
+        }
+
+        if (nextRoom != null)
+        {
+            player.Room = nextRoom;
+            _outputService.WriteLine($"{player.Name} has entered {player.Room.Name}.\n{player.Room.Description}");
+
+            foreach (var monster in player.Room.Monsters)
+            {
+                _outputService.WriteLine($"{monster.Name} is here.");
+            }
+        }
+        else
+        {
+            _outputService.WriteLine($"{player.Name} cannot move to the specified room.");
         }
     }
 }
