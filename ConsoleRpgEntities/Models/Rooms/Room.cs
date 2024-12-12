85f896edcc53faa4aa47ea557a329b76c53a19c4
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-12-11 20:22:27 -0500

    Update Room.cs

--- ConsoleRpgEntities/Models/Rooms/Room.cs
+++ ConsoleRpgEntities/Models/Rooms/Room.cs
@@ -1,5 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations.Schema;
 using ConsoleRpgEntities.Models.Characters;
+using ConsoleRpgEntities.Models.Characters.Monsters;
 
 namespace ConsoleRpgEntities.Models.Rooms
 {
@@ -25,9 +26,8 @@ namespace ConsoleRpgEntities.Models.Rooms
         public virtual Room? West { get; set; }
         public int? WestId { get; set; }
 
-
         public virtual int? PlayerId { get; set; }
         public virtual ICollection<Player> Players { get; set; }
-
+        public virtual ICollection<Monster> Monsters { get; set; }
     }
 }
