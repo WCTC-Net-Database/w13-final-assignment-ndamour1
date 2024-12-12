940fd58ed8e87d8efee4316577026bf034bfea5d
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-12-11 20:20:27 -0500

    Update Monster.cs

--- ConsoleRpgEntities/Models/Characters/Monsters/Monster.cs
+++ ConsoleRpgEntities/Models/Characters/Monsters/Monster.cs
@@ -1,14 +1,33 @@
-﻿using ConsoleRpgEntities.Models.Attributes;
+using ConsoleRpgEntities.Models.Abilities.PlayerAbilities;
+using ConsoleRpgEntities.Models.Attributes;
+using ConsoleRpgEntities.Models.Equipments;
+using ConsoleRpgEntities.Models.Rooms;
 
 namespace ConsoleRpgEntities.Models.Characters.Monsters
 {
-    public abstract class Monster : IMonster, ITargetable
+    public class Monster : IMonster, ITargetable
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int Health { get; set; }
         public int AggressionLevel { get; set; }
-        public string MonsterType { get; set; }
-        
+        public string Race { get; set; }
+        public int? Sneakiness { get; set; }
+        public int? Waaagh { get; set; }
+
+        // Foreign key
+        public int? EquipmentId { get; set; }
+
+        // Navigational properties
+        public virtual string? Class { get; set; }
+        public virtual ICollection<Ability>? Abilities { get; set; }
+        public virtual Equipment? Equipment { get; set; }
+        public virtual Room Room { get; set; }
+        public virtual int? RoomId { get; set; }
+
+        protected Monster()
+        {
+
+        }
     }
 }
