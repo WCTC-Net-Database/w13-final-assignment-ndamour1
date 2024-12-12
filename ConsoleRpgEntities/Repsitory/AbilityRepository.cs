fc2fa4368d67fe0e788f2046b4d5818a2f108726
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-12-11 20:25:55 -0500

    Additional Repositories

---
+++ ConsoleRpgEntities/Repsitory/AbilityRepository.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleRpgEntities.Data;
+using ConsoleRpgEntities.Models.Abilities.PlayerAbilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRpgEntities.Repository
+{
+    public class AbilityRepository
+    {
+        private readonly GameContext _context;
+        public AbilityRepository(GameContext context) { _context = context; }
+
+        // Create a new ability
+        public void AddAbility(Ability ability)
+        {
+            _context.Abilities.Add(ability);
+            _context.SaveChanges();
+        }
+
+        // Update an ability
+        public void UpdateAbility(Ability ability)
+        {
+            _context.Abilities.Update(ability);
+            _context.SaveChanges();
+        }
+    }
+}
