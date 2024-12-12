73e304b56b147bcbbd9e8cc25c3bddfedf3a3187
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-12-11 20:17:35 -0500

    Update IPlayer.cs

--- ConsoleRpgEntities/Models/Characters/IPlayer.cs
+++ ConsoleRpgEntities/Models/Characters/IPlayer.cs
@@ -1,5 +1,6 @@
-﻿using ConsoleRpgEntities.Models.Abilities.PlayerAbilities;
+using ConsoleRpgEntities.Models.Abilities.PlayerAbilities;
 using ConsoleRpgEntities.Models.Equipments;
+using ConsoleRpgEntities.Models.Rooms;
 
 namespace ConsoleRpgEntities.Models.Characters;
 
@@ -7,8 +8,10 @@ public interface IPlayer
 {
     int Id { get; set; }
     string Name { get; set; }
+    string Class { get; set; }
+    int Health { get; set; }
     ICollection<Ability> Abilities { get; set; }
     Inventory Inventory { get; set; }
     Equipment Equipment { get; set; }
+    Room Room { get; set; }
 }
-
