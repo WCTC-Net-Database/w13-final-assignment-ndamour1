1f60e6b13c5e3a52a08e918cd4767a657fcd2748
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-12-11 20:20:45 -0500

    Update IMonster.cs

--- ConsoleRpgEntities/Models/Characters/Monsters/IMonster.cs
+++ ConsoleRpgEntities/Models/Characters/Monsters/IMonster.cs
@@ -1,7 +1,12 @@
-﻿namespace ConsoleRpgEntities.Models.Characters.Monsters;
+using ConsoleRpgEntities.Models.Rooms;
+
+namespace ConsoleRpgEntities.Models.Characters.Monsters;
 
 public interface IMonster
 {
     int Id { get; set; }
     string Name { get; set; }
+    string Race { get; set; }
+    int Health { get; set; }
+    Room Room { get; set; }
 }
