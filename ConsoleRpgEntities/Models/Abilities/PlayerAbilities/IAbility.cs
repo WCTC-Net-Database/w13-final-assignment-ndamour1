198d0a9bdf421438b6206253540a95f5471605e7
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-12-11 20:16:00 -0500

    Update IAbility.cs

--- ConsoleRpgEntities/Models/Abilities/PlayerAbilities/IAbility.cs
+++ ConsoleRpgEntities/Models/Abilities/PlayerAbilities/IAbility.cs
@@ -6,5 +6,10 @@ public interface IAbility
 {
     int Id { get; set; }
     string Name { get; set; }
+    int Damage { get; set; }
+    int Defense { get; set; }
+    int? Distance { get; set; }
+    bool Dodge { get; set; }
+    bool InUse { get; set; }
     ICollection<Player> Players { get; set; }
 }
