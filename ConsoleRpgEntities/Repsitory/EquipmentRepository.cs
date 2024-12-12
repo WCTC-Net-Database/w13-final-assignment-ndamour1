dd452f2b7efb11a1caff3983d9bed30c15babcdb
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-12-12 16:12:46 -0500

    Additional Repository

---
+++ ConsoleRpgEntities/Repsitory/EquipmentRepository.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleRpgEntities.Data;
+using ConsoleRpgEntities.Models.Equipments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRpgEntities.Repsitory
+{
+    public class EquipmentRepository
+    {
+        private readonly GameContext _context;
+        public EquipmentRepository(GameContext context) { _context = context; }
+
+        // Update an equipment list
+        public void UpdateEquipmentList(Equipment equipment)
+        {
+            _context.Equipments.Update(equipment);
+            _context.SaveChanges();
+        }
+    }
+}
